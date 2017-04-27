334f2019d9a1832d68a9b4941895172a384dfc1a
Govind Salinas <govind@phantom.us>
2017-04-26 18:58:24 -0700

    Repository split. Creating ciscoise app repo.

---
+++ ciscoise_connector.py
@@ -0,0 +1,383 @@
+# --
+# File: ciscoise_connector.py
+#
+# Copyright (c) Phantom Cyber Corporation, 2014-2016
+#
+# This unpublished material is proprietary to Phantom Cyber.
+# All rights reserved. The methods and
+# techniques described herein are considered trade secrets
+# and/or confidential. Reproduction or distribution, in whole
+# or in part, is forbidden except by express written permission
+# of Phantom Cyber.
+#
+# --
+
+# Phantom imports
+import phantom.app as phantom
+
+from phantom.base_connector import BaseConnector
+from phantom.action_result import ActionResult
+
+# THIS Connector imports
+from ciscoise_consts import *
+from cerberus import Validator
+
+import xmltodict
+import requests
+from requests.auth import HTTPBasicAuth
+
+requests.packages.urllib3.disable_warnings()
+
+
+class CiscoISEConnector(BaseConnector):
+
+    # actions supported by this script
+    ACTION_ID_LIST_SESSIONS = "list_sessions"
+    ACTION_ID_TERMINATE_SESSION = "terminate_session"
+    ACTION_ID_LOGOFF_SYSTEM = "logoff_system"
+    ACTION_ID_QUARANTINE_SYSTEM = "quarantine_device"
+    ACTION_ID_UNQUARANTINE_SYSTEM = "unquarantine_device"
+
+    def __init__(self):
+
+        # Call the BaseConnectors init first
+        super(CiscoISEConnector, self).__init__()
+
+        self._base_url = None
+        self._auth = None
+
+    def initialize(self):
+
+        config = self.get_config()
+
+        self._auth = HTTPBasicAuth(config[phantom.APP_JSON_USERNAME], config[phantom.APP_JSON_PASSWORD])
+        self._base_url = 'https://{0}'.format(config[phantom.APP_JSON_DEVICE])
+
+        return phantom.APP_SUCCESS
+
+    def _call_rest_api(self, endpoint, action_result, schema=None, allow_unknown=True):
+
+        url = '{0}{1}'.format(self._base_url, endpoint)
+        ret_data = None
+
+        self.debug_print("REST Endpoint: ", url)
+
+        config = self.get_config()
+        verify = config[phantom.APP_JSON_VERIFY]
+
+        try:
+            resp = requests.get(url, verify=verify, auth=self._auth)
+        except Exception as e:
+            return (action_result.set_status(phantom.APP_ERROR, CISCOISE_ERR_REST_API, e), ret_data)
+
+        self.debug_print("status_code", resp.status_code)
+
+        if (resp.status_code != 200):
+            return (action_result.set_status(phantom.APP_ERROR, CISCOISE_ERR_REST_API_ERR_CODE, code=resp.status_code, message=resp.text), ret_data)
+
+        action_result.add_debug_data(resp.text)
+
+        xml = resp.text
+
+        try:
+            response_dict = xmltodict.parse(xml)
+        except Exception as e:
+            return (action_result.set_status(phantom.APP_ERROR, CISCOISE_ERR_UNABLE_TO_PARSE_REPLY, e), ret_data)
+
+        ret_data = response_dict
+
+        if (schema is not None):
+            v = Validator(schema, allow_unknown=allow_unknown)
+            if (v.validate(ret_data) is False):
+                action_result.set_status(phantom.APP_ERROR, CISCOISE_ERR_UNABLE_TO_PARSE_REPLY)
+                action_result.append_to_message(v.errors)
+                return (action_result.get_status(), ret_data)
+
+        return (phantom.APP_SUCCESS, ret_data)
+
+    def _list_sessions(self, param):
+
+        ret_val = phantom.APP_SUCCESS
+
+        action_result = self.add_action_result(ActionResult(dict(param)))
+
+        summary = action_result.update_summary({CISCOISE_JSON_TOTAL_SESSIONS: 0})
+
+        ret_data = None
+
+        ret_val, ret_data = self._call_rest_api(ACTIVE_LIST_REST, action_result)
+
+        if (phantom.is_fail(ret_val)):
+            return action_result.get_status()
+
+        self.debug_print("ret_data", ret_data)
+
+        if ('activeList' not in ret_data):
+            return action_result.set_status(phantom.APP_SUCCESS)
+
+        active_sessions = ret_data['activeList'].get('activeSession')
+
+        if (active_sessions is None):
+            return action_result.set_status(phantom.APP_SUCCESS)
+
+        # Convert the dict into list, so the rest of the code is the same
+        if (isinstance(active_sessions, dict)):
+            act_sess_list = []
+            act_sess_list.append(active_sessions)
+            active_sessions = act_sess_list
+
+        for session in active_sessions:
+
+            action_result.add_data(session)
+
+            # Init the value of the quarantine status of the session to unknown
+            session['is_quarantined'] = "Unknown"
+
+            # Get the quarantined state of the mac address
+            is_quarantined_rest = "{0}/{1}".format(IS_MAC_QUARANTINED_REST, session['calling_station_id'])
+
+            ret_val, ret_data = self._call_rest_api(is_quarantined_rest, action_result, IS_MAC_QUARAN_RESP_SCHEMA)
+
+            if (phantom.is_fail(ret_val)):
+                continue
+
+            # Can safely access the members of ret_data, since they have been parsed as by the rules of
+            # IS_MAC_QUARAN_RESP_SCHEMA
+            session['is_quarantined'] = "Yes" if ret_data["EPS_RESULT"]["userData"] == "true" else "No"
+
+        summary.update({CISCOISE_JSON_TOTAL_SESSIONS: len(active_sessions)})
+
+        return action_result.set_status(phantom.APP_SUCCESS)
+
+    def _quarantine_system(self, param):
+
+        ret_val = phantom.APP_SUCCESS
+
+        action_result = self.add_action_result(ActionResult(dict(param)))
+
+        ret_data = None
+
+        mac_ip_address = param[phantom.APP_JSON_IP_MACADDRESS]
+
+        if (phantom.is_mac(mac_ip_address)):
+            endpoint = '{0}/{1}'.format(QUARANTINE_MAC_REST, mac_ip_address)
+        elif (phantom.is_ip(mac_ip_address)):
+            endpoint = '{0}/{1}'.format(QUARANTINE_IP_REST, mac_ip_address)
+        else:
+            return action_result.set_status(phantom.APP_ERROR, CISCOISE_ERR_MAC_AND_IP_NOT_SPECIFIED)
+
+        ret_val, ret_data = self._call_rest_api(endpoint, action_result, QUARANTINE_RESP_SCHEMA)
+
+        if (phantom.is_fail(ret_val)):
+            return action_result.get_status()
+
+        action_result.add_data(ret_data)
+
+        # Can safely access the members of ret_data, since they have been parsed as by the rules of
+        # QUARANTINE_RESP_SCHEMA
+        status = ret_data['EPS_RESULT']["status"]
+
+        if (status == "Failure"):
+            return action_result.set_status(phantom.APP_ERROR, CISCOISE_ERR_ACTION_FAILED, error_code=ret_data['EPS_RESULT']['errorCode'])
+
+        # In cases where the radius authentication failed, the status is STILL set to success,
+        # but failureType and failureMessage keys are added to the ret_data, so need to check for those
+        failure_type = phantom.get_value(ret_data['EPS_RESULT'], 'failureType')
+        failure_msg = phantom.get_value(ret_data['EPS_RESULT'], 'failureMessage')
+
+        if ((failure_type is not None) or (failure_msg is not None)):
+            action_result.set_status(phantom.APP_ERROR, CISCOISE_ERR_ACTION_FAILED, error_code=ret_data['EPS_RESULT']['errorCode'])
+            if (failure_type is not None):
+                action_result.append_to_message(failure_type)
+            if (failure_msg is not None):
+                action_result.append_to_message(failure_msg)
+            return action_result.get_status()
+
+        return action_result.set_status(phantom.APP_SUCCESS, CISCOISE_SUCC_SYSTEM_QUARANTINED)
+
+    def _unquarantine_system(self, param):
+
+        ret_val = phantom.APP_SUCCESS
+
+        action_result = self.add_action_result(ActionResult(dict(param)))
+
+        ret_data = None
+
+        mac_ip_address = param[phantom.APP_JSON_IP_MACADDRESS]
+
+        if (phantom.is_mac(mac_ip_address)):
+            endpoint = '{0}/{1}'.format(UNQUARANTINE_MAC_REST, mac_ip_address)
+        elif (phantom.is_ip(mac_ip_address)):
+            endpoint = '{0}/{1}'.format(UNQUARANTINE_IP_REST, mac_ip_address)
+        else:
+            return action_result.set_status(phantom.APP_ERROR, CISCOISE_ERR_MAC_AND_IP_NOT_SPECIFIED)
+
+        ret_val, ret_data = self._call_rest_api(endpoint, action_result, QUARANTINE_RESP_SCHEMA)
+
+        if (phantom.is_fail(ret_val)):
+            return action_result.get_status()
+
+        action_result.add_data(ret_data)
+
+        status = ret_data['EPS_RESULT']["status"]
+
+        if (status == "Failure"):
+            return action_result.set_status(phantom.APP_ERROR, CISCOISE_ERR_ACTION_FAILED, error_code=ret_data['EPS_RESULT']['errorCode'])
+
+        # In cases where the radius authentication failed, the status is STILL set to success,
+        # but failureType and failureMessage keys are added to the ret_data, so need to check for those
+        failure_type = phantom.get_value(ret_data['EPS_RESULT'], 'failureType')
+        failure_msg = phantom.get_value(ret_data['EPS_RESULT'], 'failureMessage')
+
+        if ((failure_type is not None) or (failure_msg is not None)):
+            action_result.set_status(phantom.APP_ERROR, CISCOISE_ERR_ACTION_FAILED, error_code=ret_data['EPS_RESULT']['errorCode'])
+            if (failure_type is not None):
+                action_result.append_to_message(failure_type)
+            if (failure_msg is not None):
+                action_result.append_to_message(failure_msg)
+            return action_result.get_status()
+
+        return action_result.set_status(phantom.APP_SUCCESS, CISCOISE_SUCC_SYSTEM_UNQUARANTINED)
+
+    def _logoff_system(self, param):
+
+        ret_val = phantom.APP_SUCCESS
+
+        action_result = self.add_action_result(ActionResult(dict(param)))
+
+        ret_data = None
+
+        server = param[CISCOISE_JSON_SERVER]
+        mac_address = param[CISCOISE_JSON_MACADDR]
+        port = 2  # 0 is default, 1 is bounce, 2 is shutdown
+
+        endpoint = '{0}/{1}/{2}/{3}'.format(REAUTH_MAC_REST, server, mac_address, port)
+
+        ret_val, ret_data = self._call_rest_api(endpoint, action_result)
+
+        if (phantom.is_fail(ret_val)):
+            return action_result.get_status()
+
+        action_result.add_data(ret_data)
+
+        remote_coa = ret_data.get('remoteCoA')
+
+        if (remote_coa is None):
+            return action_result.set_status(phantom.APP_ERROR, CISCOISE_ERR_PARSE_REPLY)
+
+        result = remote_coa.get('results')
+
+        if (result is None):
+            return action_result.set_status(phantom.APP_ERROR, CISCOISE_ERR_PARSE_REPLY)
+
+        if (result == "false"):
+            return action_result.set_status(phantom.APP_ERROR, CISCOISE_ERR_LOGOFF_SYSTEM)
+
+        return action_result.set_status(phantom.APP_SUCCESS)
+
+    def _terminate_session(self, param):
+
+        ret_val = phantom.APP_SUCCESS
+
+        action_result = self.add_action_result(ActionResult(dict(param)))
+
+        ret_data = None
+
+        mac_address = param[phantom.APP_JSON_MACADDRESS]
+        port = 2  # 0 is default, 1 is bounce, 2 is shutdown
+
+        # First try to find the server that we should use
+        endpoint = '{0}/{1}'.format(MAC_SESSION_DETAILS_REST, mac_address)
+
+        ret_val, ret_data = self._call_rest_api(endpoint, action_result, MAC_SESSION_RESP_SCHEMA)
+
+        if (phantom.is_fail(ret_val)):
+            return action_result.get_status()
+
+        acs_server = ret_data['sessionParameters']['acs_server']
+
+        # now terminate the session
+        endpoint = '{0}/{1}/{2}/{3}'.format(DISCONNECT_MAC_REST, acs_server, mac_address, port)
+
+        ret_val, ret_data = self._call_rest_api(endpoint, action_result)
+
+        if (phantom.is_fail(ret_val)):
+            return action_result.get_status()
+
+        remote_coa = ret_data.get('remoteCoA')
+
+        if (remote_coa is None):
+            return action_result.set_status(phantom.APP_ERROR, CISCOISE_ERR_PARSE_REPLY)
+
+        result = remote_coa.get('results')
+
+        if (result is None):
+            return action_result.set_status(phantom.APP_ERROR, CISCOISE_ERR_PARSE_REPLY)
+
+        if (result == "false"):
+            return action_result.set_status(phantom.APP_ERROR, CISCOISE_ERR_TERMINATE_SESSION)
+
+        return action_result.set_status(phantom.APP_SUCCESS, CISCOISE_SUCC_SESSION_TERMINATED)
+
+    def _test_connectivity(self, param):
+
+        rest_endpoint = '{0}/{1}'.format(self._base_url, ACTIVE_COUNT_REST_ENDPOINT)
+
+        config = self.get_config()
+
+        self.save_progress(phantom.APP_PROG_CONNECTING_TO_ELLIPSES, config[phantom.APP_JSON_DEVICE])
+
+        verify = config[phantom.APP_JSON_VERIFY]
+
+        try:
+            resp = requests.get(rest_endpoint, auth=self._auth, verify=verify)
+        except Exception as e:
+            return self.set_status(phantom.APP_ERROR, CISCOISE_ERR_TEST_CONNECTIVITY_FAILED, e)
+
+        if (resp.status_code != 200):
+            return self.set_status(phantom.APP_ERROR, CISCOISE_ERR_TEST_CONNECTIVITY_FAILED_ERR_CODE, code=resp.status_code)
+
+        return self.set_status_save_progress(phantom.APP_SUCCESS, CISCOISE_SUCC_TEST_CONNECTIVITY_PASSED)
+
+    def handle_action(self, param):
+
+        result = None
+        action = self.get_action_identifier()
+
+        if (action == phantom.ACTION_ID_TEST_ASSET_CONNECTIVITY):
+            result = self._test_connectivity(param)
+        elif (action == self.ACTION_ID_LIST_SESSIONS):
+            result = self._list_sessions(param)
+        elif (action == self.ACTION_ID_TERMINATE_SESSION):
+            result = self._terminate_session(param)
+        elif (action == self.ACTION_ID_LOGOFF_SYSTEM):
+            result = self._logoff_system(param)
+        elif (action == self.ACTION_ID_QUARANTINE_SYSTEM):
+            result = self._quarantine_system(param)
+        elif (action == self.ACTION_ID_UNQUARANTINE_SYSTEM):
+            result = self._unquarantine_system(param)
+
+        return result
+
+if __name__ == '__main__':
+
+    import sys
+    import json
+    import pudb
+    pudb.set_trace()
+
+    if (len(sys.argv) < 2):
+        print "No test json specified as input"
+        exit(0)
+
+    with open(sys.argv[1]) as f:
+        in_json = f.read()
+        in_json = json.loads(in_json)
+        print(json.dumps(in_json, indent=4))
+
+        connector = CiscoISEConnector()
+        connector.print_progress_message = True
+        ret_val = connector._handle_action(json.dumps(in_json), None)
+        print json.dumps(json.loads(ret_val), indent=4)
+
+    exit(0)
---
+++ ciscoise_consts.py
@@ -0,0 +1,56 @@
+# --
+# File: ciscoise_consts.py
+#
+# Copyright (c) Phantom Cyber Corporation, 2014-2016
+#
+# This unpublished material is proprietary to Phantom Cyber.
+# All rights reserved. The methods and
+# techniques described herein are considered trade secrets
+# and/or confidential. Reproduction or distribution, in whole
+# or in part, is forbidden except by express written permission
+# of Phantom Cyber.
+#
+# --
+
+# Jsons
+CISCOISE_JSON_TYPE = "type"
+CISCOISE_JSON_SERVER = "server"
+CISCOISE_JSON_MACADDR = "mac_address"
+CISCOISE_JSON_TOTAL_SESSIONS = "sessions_found"
+CISCOISE_JSON_ENDPOINT_IP = "endpoint_ip"
+CISCOISE_JSON_NAS_IP = "nas_ip"
+
+# REST endpoint Consts
+ACTIVE_COUNT_REST_ENDPOINT = "/admin/API/mnt/Session/ActiveCount"
+ACTIVE_LIST_REST = "/admin/API/mnt/Session/ActiveList"
+AUTH_LIST_REST_ENDPOINT = "/ise/mnt/Session/AuthList/null/null"
+DISCONNECT_MAC_REST = "/ise/mnt/CoA/Disconnect"
+REAUTH_MAC_REST = "/ise/mnt/CoA/Reauth"
+IS_MAC_QUARANTINED_REST = "/ise/eps/isQuarantineByMAC"
+QUARANTINE_MAC_REST = "/ise/eps/QuarantineByMAC_S"
+QUARANTINE_IP_REST = "/ise/eps/QuarantineByIP_S"
+UNQUARANTINE_MAC_REST = "/ise/eps/UnQuarantineByMAC_S"
+UNQUARANTINE_IP_REST = "/ise/eps/UnQuarantineByIP_S"
+MAC_SESSION_DETAILS_REST = "/ise/mnt/Session/MACAddress"
+
+# Error/Success
+CISCOISE_ERR_TEST_CONNECTIVITY_FAILED = "Test connectivity failed"
+CISCOISE_ERR_TEST_CONNECTIVITY_FAILED_ERR_CODE = "Test connectivity failed with status code: '{code}'"
+CISCOISE_SUCC_TEST_CONNECTIVITY_PASSED = "Test connectivity passed"
+CISCOISE_ERR_REST_API = "REST Api error"
+CISCOISE_ERR_REST_API_ERR_CODE = "REST Api error with status code: {code}, Message from server: {message}"
+CISCOISE_ERR_UNABLE_TO_PARSE_REPLY = "Parsing error, Unable to convert xml reply to json"
+CISCOISE_SUCC_SESSION_TERMINATED = "Session terminated"
+CISCOISE_ERR_PARSE_REPLY = "Error parsing reply"
+CISCOISE_ERR_TERMINATE_SESSION = "Session termination failed. Session possibly not found"
+CISCOISE_ERR_LOGOFF_SYSTEM = "System Logoff failed"
+CISCOISE_ERR_MAC_AND_IP_NOT_SPECIFIED = "Please specify a valid mac or ip address to execute action"
+CISCOISE_ERR_ACTION_FAILED = "Action failed with error code: {error_code}"
+CISCOISE_SUCC_SYSTEM_QUARANTINED = "System Quarantined"
+CISCOISE_SUCC_SYSTEM_UNQUARANTINED = "System unquarantined"
+
+# Json reply schema
+IS_MAC_QUARAN_RESP_SCHEMA = {"EPS_RESULT": {"type": "dict", "schema": {"status": {"type": "string"}, "userData": {"type": "string"}}}}
+QUARANTINE_RESP_SCHEMA = {"EPS_RESULT": {"type": "dict", "schema": {"status": {"type": "string"}, "errorCode": {"type": "string"}}}}
+MAC_SESSION_RESP_SCHEMA = {"sessionParameters": {"type": "dict", "schema": {"acs_server": {"type": "string"}}}}
+DISCONNECT_MAC_SESS_RESP_SCHEMA = {"remoteCoA": {"type": "dict", "schema": {"results": {"type": "string"}}}}
