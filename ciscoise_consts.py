1f37976112282338e7824a5e78486c9c1c9d997e
pdesai-crest <pdesai@splunk.com>
2021-08-19 16:02:43 +0530

    lisence change

--- ciscoise_consts.py
+++ ciscoise_consts.py
@@ -1,16 +1,9 @@
 # --
 # File: ciscoise_consts.py
+# Copyright (c) 2019-2021 Splunk Inc.
 #
-# Copyright (c) Phantom Cyber Corporation, 2014-2018
-#
-# This unpublished material is proprietary to Phantom Cyber.
-# All rights reserved. The methods and
-# techniques described herein are considered trade secrets
-# and/or confidential. Reproduction or distribution, in whole
-# or in part, is forbidden except by express written permission
-# of Phantom Cyber.
-#
-# --
+# SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
+# without a valid written license from Splunk Inc. is PROHIBITED.
 
 # Jsons
 CISCOISE_JSON_SERVER = "server"
